68e804f979bac1b0a782fa7071e0847e1f1cf33a
nhi1002 <nguyennhibentre6868@gmail.com>
2019-11-28 15:02:31 +0700

    add

---
+++ add/add/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 28/11/2019
+ * Time: 2:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace add
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
